bdea534d9b3554862f8ae2354ead08b4be3068e9
regie <regie.yoonet@gmail.com>
2026-08-06 08:17:01 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const EMBEDDED_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzO5yEvRtj7mCJtbSsHvacfsndSuk5oXX_xl75Hr5r-Dm1t35JAPR9RckCGNyXG7VE_hQ/exec';
+export const EMBEDDED_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyzv1FT_dqQil5g-ai0sEih-ZcmDbCPjHxLm0j6VAN0YozUGWejhC-dq6P4Z7HWaw-TuA/exec';
